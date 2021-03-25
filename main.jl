4447fc068e47f802d69649b44c36019b90d1567e
Andreas Haller <halleritis@gmail.com>
2021-03-25 15:41:00 +0100

    small changes and a bit of commenting

--- main.jl
+++ main.jl
@@ -7,16 +7,19 @@ using Revise
 
 includet("buildLatticeOperators.jl")
 
-spin = 0.5
-L = [4, 4]
-do_project = false
-projZ = prod(L)/2-5
+spin = 0.5  # which local spin operator to take
+L = [5, 4]  # defines the Lx, Ly cell
+do_project = true  # project onto U(1) subspace, given by ...
+projZ = -prod(L)/2+10  # the projection of the magnetization along the z-axis
+# x, y, z couplings of the Heisenberg Hamiltonian
 J₁ = -1.
 J₂ = -1.
 J₃ = -1.
+# how many eigenvalues to take
 nev = 20
+# which tolerance in the eigenvalue problem
 tol = 1e-12
 
-@time buildLatticeOperators(L, spin, nev, tol, J₁, J₂, J₃, do_project=do_project, projZ=projZ)
+@time λ, ϕ = buildLatticeOperators(L, spin, nev, tol, J₁, J₂, J₃, do_project=do_project, projZ=projZ)
 
 0;
